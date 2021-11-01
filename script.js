9b0b1c168766ce211ba97d7c7de879475ca7ff02
hackzak <zakariya.ozkaraca@gmail.com>
2021-11-01 14:18:16 +0300

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+let StartGame = true;
+let ClosGame = false;
+
+let LaFrase = prompt("indiquer votre non");
+console.log(LaFrase);
+
+let LeGame = prompt("indiquer votre age");
+console.log(LeGame);
